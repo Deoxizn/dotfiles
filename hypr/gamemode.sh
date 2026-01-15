e24ddeb5a076d1a2bc157e5a744c87c2b7e5d625
Devi Garcia <asphyxiated.god@gmail.com>
2026-01-14 23:22:22 -0700

    updated gamemode.sh

--- hypr/gamemode.sh
+++ hypr/gamemode.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 HYPRGAMEMODE=$(hyprctl getoption animations:enabled | awk 'NR==1{print $2}')
 if [ "$HYPRGAMEMODE" = 1 ] ; then
+    # Apply gamemode settings directly
     hyprctl --batch "\
         keyword animations:enabled 0;\
         keyword animation borderangle,0; \
@@ -10,7 +11,16 @@ if [ "$HYPRGAMEMODE" = 1 ] ; then
         keyword general:gaps_in 0;\
         keyword general:gaps_out 0;\
         keyword general:border_size 1;\
-        keyword decoration:rounding 0"
+        keyword decoration:rounding 0;\
+        keyword decoration:active_opacity 1;\
+        keyword decoration:inactive_opacity 1;\
+        keyword general:col.active_border rgb(ffffff);\
+        keyword general:col.inactive_border rgb(ffffff)"
+    
+    # Force opacity 1 on everything
+    hyprctl windowrule opacity 1 1, match:class .*
+    hyprctl windowrule opacity 1 1, match:title .*
+    
     hyprctl notify 1 5000 "rgb(40a02b)" "Gamemode [ON]"
     exit
 else
