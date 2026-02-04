d1b06f25aa1ae10fcdaba124314295a9447a1df1
Devi <asphyxiated.god@gmail.com>
2026-02-04 07:05:29 -0700

    extend weather timeout

--- hypr/scripts/weather.sh
+++ hypr/scripts/weather.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # ~/.config/hypr/scripts/weather.sh
-result=$(timeout 3 curl -s "wttr.in/Sierra+Vista,Arizona?format=%c%t" 2>/dev/null | sed 's/+//')
+result=$(timeout 6 curl -s "wttr.in/Sierra+Vista,Arizona?format=%c%t" 2>/dev/null | sed 's/+//')
 if [ -z "$result" ]; then
     echo "🌤️ N/A"
 else
