ebba5e61adf065abe1d1f9e2874816f81596f8eb
Devi Garcia <asphyxiated.god@gmail.com>
2026-08-11 16:13:54 -0700

    remove hyprpaper for QS plugin

--- hypr/autostart.lua
+++ hypr/autostart.lua
@@ -1,5 +1,5 @@
 -- Extra autostart processes
 hl.on("hyprland.start", function()
-  hl.exec_cmd("uwsm-app -- hyprpaper")
   hl.exec_cmd("/home/deoxizn/appimg/openrgb/OpenRGB_1.0rc3_x86_64_6fbcf62.AppImage --profile \"deoxizn\" --startminimized")
+  hl.exec_cmd("quickshell -n -p /home/deoxizn/.config/quickshell/bar")
 end)
