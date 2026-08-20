07bd35c74f560f0143561cdb320eee651db2b0be
Devi Garcia <asphyxiated.god@gmail.com>
2026-08-20 15:34:43 -0700

    hypr: openrgb apply-and-exit autostart; caelestia: hide xgps/xgpsspeed from launcher; add workspace dot filter patch

--- hypr/autostart.lua
+++ hypr/autostart.lua
@@ -1,7 +1,7 @@
 -- Extra autostart processes
 hl.on("hyprland.start", function()
   hl.exec_cmd("uwsm-app -- hyprpaper")
-  hl.exec_cmd("/home/deoxizn/appimg/openrgb/OpenRGB_1.0rc3_x86_64_6fbcf62.AppImage --profile \"deoxizn\" --startminimized")
+  hl.exec_cmd("/home/deoxizn/appimg/openrgb/OpenRGB_1.0rc3_x86_64_6fbcf62.AppImage --profile \"/home/deoxizn/.config/OpenRGB/deoxizn.orp\"")
   -- omartia-dots-remux: disabled (Caelestia provides bar)
   -- hl.exec_cmd("quickshell -n -p /home/deoxizn/.config/quickshell/bar")
 end)
